34095063f5fc783bce4c56aa2cc099b9887b6faf
Sulis38 <sulissetwati085@gmail.com>
2023-06-19 22:12:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var asset5Image = document.getElementById("asset5Image");
+if (asset5Image) {
+  asset5Image.addEventListener("click", function () {
+    var anchor = document.querySelector(
+      "[data-scroll-to='copyright2023Sulissetiawati']"
+    );
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
+
+var asset6Image = document.getElementById("asset6Image");
+if (asset6Image) {
+  asset6Image.addEventListener("click", function (e) {
+    // Please sync "My Project" to the project
+  });
+}
+
+var vector = document.getElementById("vector");
+if (vector) {
+  vector.addEventListener("click", function () {
+    var anchor = document.querySelector("[data-scroll-to='aboutMeText']");
+    if (anchor) {
+      anchor.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+  });
+}
